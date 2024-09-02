84b31a286a4bb97babe24f150ec80c385abbb0ee
dawoodkaleem <dawood9743@gmail.com>
2024-09-02 21:06:30 +0500

    New Features Added

--- src/components/User/Github/Github.jsx
+++ src/components/User/Github/Github.jsx
@@ -3,7 +3,7 @@ import { useLoaderData } from "react-router-dom"
 
 
 function Github() {
-  const data = useLoaderData()
+  // const data = useLoaderData()
   // const [data ,setData]=useState([])
 //   useEffect(()=>{
 // fetch("https://api.github.com/users/hiteshchoudhary").then(response=> response.json()).then(data=>{
