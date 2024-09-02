e53e5fe7e901433daf1a5dd93e63114390197f23
dawoodkaleem <dawood9743@gmail.com>
2024-09-02 21:03:47 +0500

    New Feture added

--- src/components/User/Github/Github.jsx
+++ src/components/User/Github/Github.jsx
@@ -12,10 +12,11 @@ function Github() {
 // })
 //   },[])
   return (
-    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl ">
-      Github followers: {data.followers}
-      <img src={data.avatar_url} alt="Git picture" width={200} />
-    </div>
+    // <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl ">
+    //   Github followers: {data.followers}
+    //   <img src={data.avatar_url} alt="Git picture" width={200} />
+    // </div>
+    <> Dwoosd</>
   )
 }
 
