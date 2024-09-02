f2a41278d1e673952f58dcd0a71772d81340c8d0
dawoodkaleem <dawood9743@gmail.com>
2024-09-02 21:10:35 +0500

    Updated All New Features

--- src/components/User/Github/Github.jsx
+++ src/components/User/Github/Github.jsx
@@ -1,9 +1,9 @@
 // import { useEffect, useState } from "react"
-// import { useLoaderData } from "react-router-dom"
+import { useLoaderData } from "react-router-dom"
 
 
 function Github() {
-  // const data = useLoaderData()
+  const data = useLoaderData()
   // const [data ,setData]=useState([])
 //   useEffect(()=>{
 // fetch("https://api.github.com/users/hiteshchoudhary").then(response=> response.json()).then(data=>{
@@ -12,18 +12,17 @@ function Github() {
 // })
 //   },[])
   return (
-    // <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl ">
-    //   Github followers: {data.followers}
-    //   <img src={data.avatar_url} alt="Git picture" width={200} />
-    // </div>
-    <> Dwoosd</>
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl ">
+      Github followers: {data.followers}
+      <img src={data.avatar_url} alt="Git picture" width={200} />
+    </div>
   )
 }
 
 export default Github
 
-// export const gitInfoLoader = async () =>{
+export const gitInfoLoader = async () =>{
 
-//  const responce= await fetch("https://api.github.com/users/hiteshchoudhary")
-//  return responce.json()
-// }
+ const responce= await fetch("https://api.github.com/users/hiteshchoudhary")
+ return responce.json()
+}
